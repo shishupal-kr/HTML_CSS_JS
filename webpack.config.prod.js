88ee176cb7fedc45a2a76209a24022eb471ddca8
Shishupal <shishupalking@gmail.com>
2025-02-04 23:53:09 +0530

    change file name

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './selector.html',
     }),
     new CopyPlugin({
       patterns: [
