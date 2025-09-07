6ba35d5f4b66e2cdeb2b0c75974284ce27d7084a
Shishupal <shishupalking@gmail.com>
2025-09-07 11:27:12 +0530

    added javascript

---
+++ js/first/first.js
@@ -0,0 +1,26 @@
+name = "shishupal"
+age = 24
+float = 24.55
+isfollow = true
+a= null
+y= undefined
+
+console.log(name);
+console.log(age);
+
+let names = "shishupalk" // value can be updated
+console.log(names);
+
+let ages = 25
+console.log(ages)
+
+const r = 14.5 // value can't be changed
+console.log(r)
+
+const student = { //object
+  name: "shishupal",
+  age : 24,
+  isPassed: true,
+};
+student ["name"] = " shishupal kr";
+console.log(name)
---
+++ js/script.js
@@ -0,0 +1,37 @@
+//arithmetic operation
+
+let a= 5;
+let b = 4;
+
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);
+
+//unary operator
+
+a = ++a;
+console.log("a=",a);
+b = --b;
+console.log("a=",b);
+
+//assignment operator
+a+=4
+console.log(a);
+a-=4
+console.log(a);
+a*=4
+console.log(a);
+
+//comparison operator
+console.log(a==b); //only chek num
+console.log(a!=b);
+console.log(a===b); //also check data type
+console.log(a>b);
+console.log(a>=b);
+console.log(a<=b);
+
+// logical operator
+console.log(a=b && a>b)
+console.log(a=b || a>b)
