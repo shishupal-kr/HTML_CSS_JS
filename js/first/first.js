010fbb049635af7080aff27da8d9916e41116c68
Shishupal <shishupalking@gmail.com>
2025-09-11 21:04:22 +0530

    added code

--- js/first/first.js
+++ js/first/first.js
@@ -2,8 +2,8 @@ name = "shishupal"
 age = 24
 float = 24.55
 isfollow = true
-a= null
-y= undefined
+a = null
+y = undefined
 
 console.log(name);
 console.log(age);
@@ -14,13 +14,21 @@ console.log(names);
 let ages = 25
 console.log(ages)
 
-const r = 14.5 // value can't be changed
+const r = 14.5 // value of const can't be changed
 console.log(r)
 
-const student = { //object
+//objects
+const student= { //const obj -> key can be changed like- age:24
   name: "shishupal",
   age : 24,
   isPassed: true,
 };
-student ["name"] = " shishupal kr";
-console.log(name)
+
+//way to print object
+console.log(student["age"])
+console.log(student["isPassed"])
+console.log(student.name)
+console.log(student.isPassed)
+
+student["age"] = student["age"] +1;
+console.log(student.age)
---
+++ js/first/question.js
@@ -0,0 +1,8 @@
+const profile = {
+  username: "@shishupalk",
+  post: 99,
+  follower: 250,
+  following: 400,
+  bio: "hey you are on my profile",
+  isfollow: false,
+};
